09398c3af483c02a4a64da0859c9d8764ca71f12
Tanish Khandelwal <76485783+tknishh@users.noreply.github.com>
2023-02-06 01:32:18 +0530

    transform

--- transform.py
+++ transform.py
@@ -10,4 +10,16 @@ def Data_quality(load_df):
     else:
         raise Exception("Primary Key Exception,Data Might Contain duplicates")
 
-        
+    if load_df.isnull().values.any():
+        raise Exception("Null values found")
+
+def Transform_df(load_df):
+    Transformed_df = load_df.groupby(['timestamp','artist_name'], as_index = False).count()
+    Transformed_df.RENAME(COLUMNS = {'played_at':'count'}, inplace = True)
+
+    Transformed_df['ID'] = Transformed_df['timestamp'].astype(str) + "-" + Transformed_df['artist_name']
+
+    return Transformed_df[['ID','timestamp','artist_name','count']]
+
+if __name__ == "__main__":
+    load_df = extract.return_dataframe()
